d6b21d54efacdc4cc3fa4bbe42d78c735b19725c
jonathan <jonathan.jibikilayi@esprit.tn>
2021-10-28 00:24:22 +0100

    test

---
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -0,0 +1,60 @@
+package tn.esprit.spring;
+
+import static org.junit.Assert.assertEquals;
+
+import java.text.ParseException;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import tn.esprit.spring.entities.Departement;
+import tn.esprit.spring.entities.Entreprise;
+import tn.esprit.spring.services.IDepartementService;
+import tn.esprit.spring.services.IEntrepriseService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EntrepriseServiceImplTest {
+	
+	
+	
+	
+	Logger logger = LoggerFactory.getLogger(EntrepriseServiceImplTest.class);
+	
+	@Autowired
+	IEntrepriseService ientrepriseservice;
+	@Autowired
+	IDepartementService idepartement;
+	
+	
+	@Test
+	public void  testAjouterEntreprise() throws ParseException{
+		
+		
+		Entreprise entreprise = new Entreprise("vitalait", "AAAAAA") ;
+		
+		int idEntreprise=ientrepriseservice.ajouterEntreprise(entreprise);
+		assertEquals(idEntreprise, entreprise.getId());
+		
+		
+		
+		 
+	}
+	
+	@Test
+	public void testAjouterDepartement(){
+		
+		
+		Departement department = new Departement("ressourceH");
+		int idDepartement = ientrepriseservice.ajouterDepartement(department);
+		assertEquals(idDepartement, department.getId());
+		
+		
+	}
+
+}
