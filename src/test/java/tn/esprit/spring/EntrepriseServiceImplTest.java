9217a183bb2eefdfd02390e48e08792a20790380
jibikilayijonathan <44599155+jibikilayijonathan@users.noreply.github.com>
2021-11-08 20:40:07 +0100

    Update EntrepriseServiceImplTest.java

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -80,7 +80,7 @@ public class EntrepriseServiceImplTest {
 	@Test
 	public void testDeleteEntrepriseById_METHOD1() {
 		try {
-		Entreprise E = new Entreprise("Samsung","EURL");
+		Entreprise E = new Entreprise("Samsungss","EURL");
 		int Id = es.ajouterEntreprise(E);
 		int lengthBeforeDelete = es.getAllEntreprises().size();
 		es.deleteEntrepriseById(Id);
