79a61d6182433a153907298c58257623304b807c
jibikilayijonathan <44599155+jibikilayijonathan@users.noreply.github.com>
2021-11-09 12:09:15 +0100

    Update EntrepriseServiceImplTest.java

--- src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
+++ src/test/java/tn/esprit/spring/EntrepriseServiceImplTest.java
@@ -63,7 +63,7 @@ public class EntrepriseServiceImplTest {
 	@Test
 	public void testUpdateEntreprise() {
 		try {
-		Entreprise E = new Entreprise("s","EURL");
+		Entreprise E = new Entreprise("samsung","EURL");
 		int Id = es.ajouterEntreprise(E);
 		E.setName("Iphone");
 		es.ajouterEntreprise(E);
